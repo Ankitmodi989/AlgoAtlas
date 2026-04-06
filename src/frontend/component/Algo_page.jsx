0a4da83786a640ef09cf6cfb9843966212c3e6ba
Ankit Modi <ankitmodi2428@gmail.com>
2026-04-07 01:12:52 +0530

    merge-quick sort added

--- src/frontend/component/Algo_page.jsx
+++ src/frontend/component/Algo_page.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import MergeSort from '../Sorting_algo/MergeSort';
+import QuickSort from '../Sorting_algo/QuickSort';
 
 const Algo_page = () => {
     const{id}=useParams();
 
-  return (
-    <div>{id}</div>
-  )
+  
+      if(id=="merge")
+        return <MergeSort></MergeSort>
+      else if(id=="quick")
+        return <QuickSort></QuickSort>
+   
+  
 }
 
 export default Algo_page
